6a83013aa8aad131b074ff90a50aa4983ceb1e69
kingstacker <kingstacker_work@163.com>
2017-11-27 11:21:35 +0800

    led display optimaztion

--- src/led_display.v
+++ src/led_display.v
@@ -13,12 +13,14 @@ module  led_display (
 /*o*/   output   wire    [2:0]     sel              ,  //38 decoder;
         output   wire    [7:0]     seg                 
 );
-assign seg = 8'b1001_0000; //9;
 parameter MS_MAX = 16'd4_9999; //1ms dynamic refresh;//4_9999;
 reg [15:0] cnt_1ms;
 reg cnt_1ms_flag;
 reg [2:0] cnt_sel;
 reg [2:0] sel_r;
+reg [6:0] seg_r;
+reg [7:0] seg_sel;
+wire dp;
 always @(posedge clk or negedge rst_n) begin
     if (~rst_n) begin
         cnt_1ms <= 0;
@@ -41,10 +43,10 @@ always @(posedge clk or negedge rst_n) begin
     end //if
     else begin
         if (cnt_sel == 3'd6) begin
-        	cnt_sel <= 0;
+            cnt_sel <= 0;
         end    
         else begin
-        	cnt_sel <= (cnt_1ms_flag == 1'b1) ? cnt_sel + 1'b1 : cnt_sel;
+            cnt_sel <= (cnt_1ms_flag == 1'b1) ? cnt_sel + 1'b1 : cnt_sel;
         end
     end //else
 end //always
@@ -54,18 +56,39 @@ always @(posedge clk or negedge rst_n) begin
         sel_r <= 3'b111;
     end //if
     else begin
-        case (cnt_sel)
-            3'd0:    begin sel_r <= 3'b101; end
-            3'd1:    begin sel_r <= 3'b100; end
-            3'd2:    begin sel_r <= 3'b011; end
-            3'd3:    begin sel_r <= 3'b010; end
-            3'd4:    begin sel_r <= 3'b001; end
-            3'd5:    begin sel_r <= 3'b000; end
-            default: begin sel_r <= 3'b111; end
+        case (cnt_sel) 
+            3'd0:    begin sel_r <= 3'b101;seg_sel <= din[7:0];   end
+            3'd1:    begin sel_r <= 3'b100;seg_sel <= din[15:8];  end
+            3'd2:    begin sel_r <= 3'b011;seg_sel <= din[23:16]; end
+            3'd3:    begin sel_r <= 3'b010;seg_sel <= din[31:24]; end
+            3'd4:    begin sel_r <= 3'b001;seg_sel <= din[39:32]; end
+            3'd5:    begin sel_r <= 3'b000;seg_sel <= din[47:40]; end
+            default: begin sel_r <= 3'b111;                       end
             endcase //case    
     end //else
 end //always
+always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        seg_r <= 7'b111_1111;
+    end //if
+    else begin
+        case (seg_sel)
+            8'd0: begin seg_r <= 7'b100_0000; end //0; 
+            8'd1: begin seg_r <= 7'b111_1001; end //1; 
+            8'd2: begin seg_r <= 7'b010_0100; end //2; 
+            8'd3: begin seg_r <= 7'b011_0000; end //3; 
+            8'd4: begin seg_r <= 7'b001_1001; end //4; 
+            8'd5: begin seg_r <= 7'b001_0010; end //5; 
+            8'd6: begin seg_r <= 7'b000_0010; end //6; 
+            8'd7: begin seg_r <= 7'b111_1000; end //7; 
+            8'd8: begin seg_r <= 7'b000_0000; end //8; 
+            8'd9: begin seg_r <= 7'b001_0000; end //9; 
+            default: begin seg_r <= 7'b111_1111; end //x;
+            endcase //case    
+    end //else
+end //always
+assign dp = !(cnt_sel == 3'd2 || cnt_sel == 3'd4);  //dp logic produce case;
 assign sel = sel_r;
-
+assign seg = {dp,seg_r};
 endmodule
 
---
+++ src/sco.v
@@ -0,0 +1,21 @@
+//************************************************
+//  Filename      : sco.v                             
+//  Author        : kingstacker                  
+//  Company       : School                       
+//  Email         : kingstacker_work@163.com     
+//  Device        : Altera cyclone4 ep4ce6f17c8  
+//  Description   :                              
+//************************************************
+module  sco (
+/*i*/   input    wire              clk                 ,
+        input    wire              rst_n               ,
+        input    wire              key_bs              , //begin and stop;
+        input    wire              key_rec             , //recording;
+        input    wire              key_dis             , //display the value;
+/*o*/   output   wire    [47:0]    dout        
+);
+
+
+
+
+endmodule
